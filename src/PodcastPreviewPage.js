34652da2e176565ea45d06c89a3e9ce41ef4dd13
ARTI1208 <leonardo906@mail.ru>
2020-09-18 09:01:21 +0300

    Preview done

--- src/PodcastPreviewPage.js
+++ src/PodcastPreviewPage.js
@@ -1,65 +1,136 @@
-import React from 'react';
-import {Panel, PanelHeaderBack} from "@vkontakte/vkui";
+import React, {createRef} from 'react';
+import {Panel, PanelHeaderBack, Separator} from "@vkontakte/vkui";
 import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
 import PanelHeaderContent from "@vkontakte/vkui/dist/components/PanelHeaderContent/PanelHeaderContent";
 import Button from "@vkontakte/vkui/dist/components/Button/Button";
 import podcastStore from "./store/PodcastStore";
 import playIcon from "./assets/ic_play.svg"
+import pauseIcon from "./assets/ic_pause.svg"
 import "./PodcastPreviewPage.css"
+import "./base.css"
 import Group from "@vkontakte/vkui/dist/components/Group/Group";
-import Header from "@vkontakte/vkui/dist/components/Header/Header";
 
-export default function PodcastPreviewPage(props) {
+export default class PodcastPreviewPage extends React.Component {
 
-    return (
-        <Panel id={"preview_panel"}>
-            <PanelHeader>
-                <PanelHeaderContent
-                    aside={<div>Новый подкаст</div>}
-                    before={<PanelHeaderBack onClick={() => props.back()} />}
-                    status={null}/>
-            </PanelHeader>
+    constructor(props) {
+        super(props);
+        this.state = {duration: "00:00"}
+    }
 
-            <Group
-                separator={'auto'}
-                header={<div>
-                    <div className={"image-preview-block"}>
-                        <img src={podcastStore.getState().imagePreview}/>
-                        <img src={playIcon}/>
-                    </div>
-                    <div className={"podcast-title-text-block"}>
-                        <span className={"block-title"}>{podcastStore.getState().title}</span>
-                        <span className={"link"}>ПапаDogs</span>
-                        <span className={"subtitle"}>Длительность: 59:16</span>
-                    </div>
-                </div>}
-            />
+    formatSeconds(secs) {
+        secs = Math.round(secs);
+        let hours = Math.floor(secs / (60 * 60));
 
-            <Group
-                separator={'auto'}
-                header={<div className={"block-title"}>Описание</div>}
-                description={<div className={"description-text"}>
-                    {podcastStore.getState().description}
-                </div>}
-            />
+        const divisor_for_minutes = secs % (60 * 60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
 
-            <Group
-                separator={'auto'}
-                header={<div className={"block-title"}>Содержание</div>}
-                description={<div>
-                    {podcastStore.getState().timecodes.map(el => {
-                        return <div style={{
-                            paddingTop: 14,
-                            paddingBottom: 14,
-                        }}>
-                            <span className={"timecode-time"}>{el.time}</span>
-                            <span className={"timecode-title"}>  —  {el.title}</span>
+        const divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+
+        // console.log(hours + ":" + minutes + ":" + seconds);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (hours < 10) {
+            if (hours < 1) {
+                return minutes + ":" + seconds;
+            }
+            hours = "0" + hours;
+        }
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+    render() {
+        const audioRef = createRef()
+        const playIconRef = createRef()
+
+        return (
+            <Panel id={"preview_panel"}>
+                <PanelHeader separator={true}>
+                    <PanelHeaderContent
+                        aside={<div>Новый подкаст</div>}
+                        before={<PanelHeaderBack onClick={() => this.props.back()}/>}
+                        status={null}/>
+
+                </PanelHeader>
+
+                <div className={"page"}>
+                    <Group
+                        separator={'hide'}
+                        children={<div className={"header-block"}>
+                            <div className={"image-preview-block"}>
+                                <img className={"image-preview"} src={podcastStore.getState().imagePreview}/>
+                                <img ref={playIconRef} className={"play-icon"} src={playIcon}
+                                     onClick={() => {
+                                         const player = audioRef.current
+                                         if (player.paused) {
+                                             player.play()
+                                         } else {
+                                             player.pause()
+                                         }
+                                     }}/>
+                                <audio
+                                    ref={audioRef}
+                                    src={podcastStore.getState().audio}
+                                    onPlay={() => {
+                                        playIconRef.current.src = pauseIcon
+                                    }}
+                                    onPause={() => {
+                                        playIconRef.current.src = playIcon
+                                    }}
+                                    onEnded={() => {
+                                        playIconRef.current.src = playIcon
+                                    }}
+                                    onDurationChange={() => this.setState({duration: this.formatSeconds(audioRef.current.duration)})}/>
+                            </div>
+                            <div className={"podcast-title-text-block"}>
+                                <span className={"block-title"}>{podcastStore.getState().title}</span>
+                                <span className={"link"}>ПапаDogs</span>
+                                <span className={"subtitle"}>Длительность: {this.state.duration}</span>
+                            </div>
                         </div>}
-                    )}
-                </div>}
-            />
+                    />
 
-            <Button stretched={true} size={'l'} onClick={() => props.next()}>Далее</Button>
-        </Panel>
-    );
+                    <Separator style={{marginTop: "12px"}} wide={true}/>
+
+                    <Group
+                        separator={'hide'}
+                        header={<div className={"block-title"}>Описание</div>}
+                        children={<div className={"description-text"}>
+                            {podcastStore.getState().description}
+                        </div>}
+                    />
+
+                    <Separator wide={true}/>
+
+                    <Group
+                        separator={'hide'}
+                        header={<div className={"block-title"}>Содержание</div>}
+                        children={<div>
+                            {podcastStore.getState().timecodes.map(el => {
+                                    return <div style={{
+                                        paddingTop: 14,
+                                        paddingBottom: 14,
+                                    }}>
+                                        <span className={"timecode-time"}>{el.time}</span>
+                                        <span className={"timecode-title"}>  — {el.title}</span>
+                                    </div>
+                                }
+                            )}
+                        </div>}
+                    />
+
+                    <Separator style={{marginBottom: "24px"}} wide={true}/>
+
+                    <Button className={"next-button"} style={{width: "100%"}} stretched={true} size={'l'}
+                            onClick={() => this.props.next()}>Далее</Button>
+                </div>
+            </Panel>
+        );
+    }
 }
--- src/store/PodcastStore.js
+++ src/store/PodcastStore.js
@@ -4,8 +4,8 @@ const podcastStore = createStore(reducer, {
     panel: "first_panel",
     title: "Title",
     description: "Description",
-    imagePreview: undefined,
-    audio: undefined,
+    imagePreview: "https://cdn.shopify.com/s/files/1/1074/8258/products/9040-99_2048x.jpg?v=1572553208",
+    audio: "https://zaycev.net/musicset/dl/5c0f3aa4eda818f24c4ec0568ad0a6fa/8773159.json?spa=false",
     adultContent: false,
     excludeFromExport: false,
     trailer: false,
@@ -18,6 +18,79 @@ const podcastStore = createStore(reducer, {
         {
             time: "9:17",
             title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "leto"
+        },
+        {
+            time: "9:17",
+            title: "oops"
         },
     ]
 });
